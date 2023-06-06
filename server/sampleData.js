328f4588d60dc87ddfced3ca7234595358fcffc4
Godfred <godfrednai@gmail.com>
2023-06-06 14:19:09 +0000

    initial commit

---
+++ server/sampleData.js
@@ -0,0 +1,71 @@
+const projects = [
+  {
+    id: "1",
+    clientId: "1",
+    name: "eCommerce Website",
+    description:
+      " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi, vero molestiae ducimus harum voluptatum ipsum explicabo qui ad doloremque exercitationem accusantium, laborum laboriosam repellendus consectetur nemo voluptates pariatur eaque ea. ",
+    status: "In Progress",
+  },
+  {
+    id: "2",
+    clientId: "2",
+    name: "TODO Website",
+    description:
+      " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi, vero molestiae ducimus harum voluptatum ipsum explicabo qui ad doloremque exercitationem accusantium, laborum laboriosam repellendus consectetur nemo voluptates pariatur eaque ea. ",
+    status: "Done",
+  },
+  {
+    id: "3",
+    clientId: "3",
+    name: "Blog App",
+    description:
+      " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi, vero molestiae ducimus harum voluptatum ipsum explicabo qui ad doloremque exercitationem accusantium, laborum laboriosam repellendus consectetur nemo voluptates pariatur eaque ea. ",
+    status: "In Progress",
+  },
+  {
+    id: "3",
+    clientId: "3",
+    name: "Mechanic Website",
+    description:
+      " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi, vero molestiae ducimus harum voluptatum ipsum explicabo qui ad doloremque exercitationem accusantium, laborum laboriosam repellendus consectetur nemo voluptates pariatur eaque ea. ",
+    status: "In Progress",
+  },
+  {
+    id: "4",
+    clientId: "4",
+    name: "Portfolio Website",
+    description:
+      " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi, vero molestiae ducimus harum voluptatum ipsum explicabo qui ad doloremque exercitationem accusantium, laborum laboriosam repellendus consectetur nemo voluptates pariatur eaque ea. ",
+    status: "In Progress",
+  },
+];
+
+const clients = [
+  {
+    id: "1",
+    name: "Tony Stark",
+    email: "ironman@gmail.com",
+    phone: "+233-557-569-078",
+  },
+  {
+    id: "2",
+    name: "Tyrion Lannister",
+    email: "theimp@gmail.com",
+    phone: "+233-557-569-078",
+  },
+  {
+    id: "3",
+    name: "Michael Scott",
+    email: "whatshesaid@gmail.com",
+    phone: "+233-456-732-554",
+  },
+  {
+    id: "4",
+    name: "Daemon Targaryen",
+    email: "rogueprince@gmail.com",
+    phone: "+233-576-448-078",
+  },
+];
+
+module.exports = { projects, clients };
