9de125be315c60d84cd5ff25c74aa4d6703b291d
Godfred <godfrednai@gmail.com>
2023-06-06 18:40:14 +0000

    added databse connection

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const colors = require("colors");
 require("dotenv").config();
 const schema = require("./schema/schema");
 const port = process.env.PORT || 5000;
 const { graphqlHTTP } = require("express-graphql");
+const connectDB = require('../server/config/db')
 
 const app = express();
+
+//connect to dadabase
+connectDB();
 
 app.use(
   "/graphql",
@@ -14,4 +19,4 @@ app.use(
   })
 );
 
-app.listen(port, console.log(`Server running on port ${port}`));
+app.listen(port, console.log(`Server running on port ${port}`.yellow));
