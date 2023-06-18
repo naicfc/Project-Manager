503abeaad1b77fd30cf42a4be9948ac89df6522a
Godfred <godfrednai@gmail.com>
2023-06-18 21:45:33 +0000

    modified spinner size and positioning

--- client/src/components/Spinner.jsx
+++ client/src/components/Spinner.jsx
@@ -2,9 +2,9 @@ import { FaSpinner } from "react-icons/fa";
 
 export default function Spinner() {
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center items-center h-[80vh]">
       <button type="button" className="text-gray-500" disabled>
-        <FaSpinner size={50}/>
+        <FaSpinner size={50} />
       </button>
     </div>
   );
