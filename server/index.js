1e14d81b37537aa24e901c052bfb55014ae01197
Godfred <godfrednai@gmail.com>
2023-06-18 21:54:17 +0000

    minor changes

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const colors = require("colors");
 require("dotenv").config();
-const cors = require('cors')
+const cors = require("cors");
 const schema = require("./schema/schema");
 const port = process.env.PORT || 5000;
 const { graphqlHTTP } = require("express-graphql");
